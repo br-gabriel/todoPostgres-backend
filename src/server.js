db0903361e3eb8507a08e4a1dec55d933fe00102
gabriel <contato.gabrielfeitosa@gmail.com>
2025-01-24 17:07:44 -0300

    cors settings update

--- src/server.js
+++ src/server.js
@@ -8,8 +8,11 @@ const app = express();
 
 app.use(cors({
     origin: process.env.ORIGIN_URL,
-    credentials: true,
-}));
+    methods: ['GET', 'POST', 'PUT', 'DELETE', 'OPTIONS'],
+    allowedHeaders: ['Content-Type', 'Authorization', 'Cookie'],
+    credentials: true
+  }));
+  
 app.use(express.json());
 app.use(cookieParser());
 app.use(todosRoutes);
