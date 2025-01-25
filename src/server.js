fec32b938e33eafdf1231a674ff57d40c3bf28b7
gabriel <contato.gabrielfeitosa@gmail.com>
2025-01-25 02:18:20 -0300

    cors update new urls

--- src/server.js
+++ src/server.js
@@ -7,23 +7,11 @@ const userRoutes = require("./routes/user.routes");
 const app = express();
 
 app.use(cors({
-    origin: process.env.ORIGIN_URL || 'https://todo-postgres-frontend.vercel.app',
+    origin: process.env.ORIGIN_URL || 'https://todo-postgres-frontend.vercel.app' || 'https://todo-postgres-frontend-git-master-brgabriels-projects.vercel.app/' || 'https://todo-postgres-frontend-atjk2xk99-brgabriels-projects.vercel.app/',
     methods: ['GET', 'POST', 'PUT', 'DELETE', 'OPTIONS'],
     allowedHeaders: ['Content-Type', 'Authorization', 'Cookie'],
     credentials: true
 }));
-
-// // Middleware para definir cabeçalhos CORS manualmente
-// app.use((req, res, next) => {
-//     res.header("Access-Control-Allow-Origin", process.env.ORIGIN_URL || 'https://todo-postgres-frontend.vercel.app');
-//     res.header("Access-Control-Allow-Methods", "GET, POST, PUT, DELETE, OPTIONS");
-//     res.header("Access-Control-Allow-Headers", "Content-Type, Authorization, Cookie");
-//     res.header("Access-Control-Allow-Credentials", "true");
-//     if (req.method === 'OPTIONS') {
-//         return res.sendStatus(200);
-//     }
-//     next();
-// });
 
 app.use(express.json());
 app.use(cookieParser());
