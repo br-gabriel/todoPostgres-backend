dc87e747c921af870e867db617a205e2863df413
gabriel <contato.gabrielfeitosa@gmail.com>
2025-01-25 02:48:40 -0300

    add cors package and use middleware for handling CORS

--- src/server.js
+++ src/server.js
@@ -1,3 +1,4 @@
+const cors = require("cors");
 const express = require("express");
 const cookieParser = require("cookie-parser");
 const todosRoutes = require("./routes/todos.routes");
@@ -5,17 +6,19 @@ const userRoutes = require("./routes/user.routes");
 
 const app = express();
 
-app.use((req, res, next) => {
-    res.header("Access-Control-Allow-Origin", process.env.ORIGIN_URL);
-    res.header("Access-Control-Allow-Methods", "GET, POST, PUT, DELETE, OPTIONS");
-    res.header("Access-Control-Allow-Headers", "Content-Type, Authorization, Cookie");
-    res.header("Access-Control-Allow-Credentials", "true");
+app.use(cors());
 
-    if (req.method === "OPTIONS") {
-        return res.sendStatus(200);
-    }
-    next();
-});
+// app.use((req, res, next) => {
+//     res.header("Access-Control-Allow-Origin", process.env.ORIGIN_URL);
+//     res.header("Access-Control-Allow-Methods", "GET, POST, PUT, DELETE, OPTIONS");
+//     res.header("Access-Control-Allow-Headers", "Content-Type, Authorization, Cookie");
+//     res.header("Access-Control-Allow-Credentials", "true");
+
+//     if (req.method === "OPTIONS") {
+//         return res.sendStatus(200);
+//     }
+//     next();
+// });
 
 app.use(express.json());
 app.use(cookieParser());
