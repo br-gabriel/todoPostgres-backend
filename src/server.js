a1664decfdcbf4594cff40074e0113aa56e7f416
gabriel <contato.gabrielfeitosa@gmail.com>
2025-01-25 02:53:48 -0300

    refactor CORS middleware to use configuration options for better flexibility

--- src/server.js
+++ src/server.js
@@ -6,19 +6,12 @@ const userRoutes = require("./routes/user.routes");
 
 const app = express();
 
-app.use(cors());
-
-// app.use((req, res, next) => {
-//     res.header("Access-Control-Allow-Origin", process.env.ORIGIN_URL);
-//     res.header("Access-Control-Allow-Methods", "GET, POST, PUT, DELETE, OPTIONS");
-//     res.header("Access-Control-Allow-Headers", "Content-Type, Authorization, Cookie");
-//     res.header("Access-Control-Allow-Credentials", "true");
-
-//     if (req.method === "OPTIONS") {
-//         return res.sendStatus(200);
-//     }
-//     next();
-// });
+app.use(cors({
+    origin: process.env.ORIGIN_URL,
+    methods: ['GET', 'POST', 'PUT', 'DELETE', 'OPTIONS'],
+    allowedHeaders: ['Content-Type', 'Authorization', 'Cookie'],
+    credentials: true
+}));
 
 app.use(express.json());
 app.use(cookieParser());
