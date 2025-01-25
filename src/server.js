f19cf66a48419cf6effcd715c649076c6740e6d5
gabriel <contato.gabrielfeitosa@gmail.com>
2025-01-25 02:28:59 -0300

    cors test

--- src/server.js
+++ src/server.js
@@ -6,16 +6,16 @@ const userRoutes = require("./routes/user.routes");
 
 const app = express();
 
-app.use(cors({
-    origin: process.env.ORIGIN_URL,
-    methods: ['GET', 'POST', 'PUT', 'DELETE', 'OPTIONS'],
-    allowedHeaders: ['Content-Type', 'Authorization', 'Cookie'],
-    credentials: true
-}));
-
 app.use(express.json());
 app.use(cookieParser());
 app.use(todosRoutes);
 app.use(userRoutes);
 
 app.listen(process.env.PORT || 3232);
+
+app.use(cors({
+    origin: process.env.ORIGIN_URL,
+    methods: ['GET', 'POST', 'PUT', 'DELETE', 'OPTIONS'],
+    allowedHeaders: ['Content-Type', 'Authorization', 'Cookie'],
+    credentials: true
+}));
