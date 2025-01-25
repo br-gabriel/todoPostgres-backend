3a90af3e739f6637e5848694bd80575f2051fdab
gabriel <contato.gabrielfeitosa@gmail.com>
2025-01-25 03:28:27 -0300

    update CORS configuration to use URL environment variable

--- src/server.js
+++ src/server.js
@@ -7,7 +7,7 @@ const userRoutes = require("./routes/user.routes");
 const app = express();
 
 app.use(cors({
-    origin: process.env.ORIGIN_URL,
+    origin: process.env.URL,
     methods: ['GET', 'POST', 'PUT', 'DELETE', 'OPTIONS'],
     allowedHeaders: ['Content-Type', 'Authorization', 'Cookie'],
     credentials: true
