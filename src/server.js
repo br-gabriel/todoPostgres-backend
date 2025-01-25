645ce624a101de21c3f6386fe3da5eef6fea6b79
gabriel <contato.gabrielfeitosa@gmail.com>
2025-01-25 02:05:50 -0300

    CORS settings update

--- src/server.js
+++ src/server.js
@@ -7,12 +7,21 @@ const userRoutes = require("./routes/user.routes");
 const app = express();
 
 app.use(cors({
-    origin: process.env.ORIGIN_URL,
+    origin: process.env.ORIGIN_URL || 'https://todo-postgres-frontend.vercel.app',
     methods: ['GET', 'POST', 'PUT', 'DELETE', 'OPTIONS'],
     allowedHeaders: ['Content-Type', 'Authorization', 'Cookie'],
     credentials: true
 }));
-  
+
+// Middleware para definir cabeçalhos CORS manualmente
+app.use((req, res, next) => {
+    res.header("Access-Control-Allow-Origin", process.env.ORIGIN_URL || 'https://todo-postgres-frontend.vercel.app');
+    res.header("Access-Control-Allow-Methods", "GET, POST, PUT, DELETE, OPTIONS");
+    res.header("Access-Control-Allow-Headers", "Content-Type, Authorization, Cookie");
+    res.header("Access-Control-Allow-Credentials", "true");
+    next();
+});
+
 app.use(express.json());
 app.use(cookieParser());
 app.use(todosRoutes);
