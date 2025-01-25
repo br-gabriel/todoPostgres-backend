decda9cdc5bcf78b8d961a26bd656cf5199d5463
gabriel <contato.gabrielfeitosa@gmail.com>
2025-01-25 02:10:18 -0300

    CORS update

--- src/server.js
+++ src/server.js
@@ -19,6 +19,9 @@ app.use((req, res, next) => {
     res.header("Access-Control-Allow-Methods", "GET, POST, PUT, DELETE, OPTIONS");
     res.header("Access-Control-Allow-Headers", "Content-Type, Authorization, Cookie");
     res.header("Access-Control-Allow-Credentials", "true");
+    if (req.method === 'OPTIONS') {
+        return res.sendStatus(200);
+    }
     next();
 });
 
