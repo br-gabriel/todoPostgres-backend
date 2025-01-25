7c78cc4beed4a8beb595664be53337eefa281390
gabriel <contato.gabrielfeitosa@gmail.com>
2025-01-25 02:37:50 -0300

    remove cors dependency and implement custom CORS middleware

--- src/server.js
+++ src/server.js
@@ -1,4 +1,3 @@
-const cors = require("cors");
 const express = require("express");
 const cookieParser = require("cookie-parser");
 const todosRoutes = require("./routes/todos.routes");
@@ -6,16 +5,21 @@ const userRoutes = require("./routes/user.routes");
 
 const app = express();
 
+app.use((req, res, next) => {
+    res.header("Access-Control-Allow-Origin", process.env.ORIGIN_URL);
+    res.header("Access-Control-Allow-Methods", "GET, POST, PUT, DELETE, OPTIONS");
+    res.header("Access-Control-Allow-Headers", "Content-Type, Authorization, Cookie");
+    res.header("Access-Control-Allow-Credentials", "true");
+
+    if (req.method === "OPTIONS") {
+        return res.sendStatus(200);
+    }
+    next();
+});
+
 app.use(express.json());
 app.use(cookieParser());
 app.use(todosRoutes);
 app.use(userRoutes);
 
 app.listen(process.env.PORT || 3232);
-
-app.use(cors({
-    origin: process.env.ORIGIN_URL,
-    methods: ['GET', 'POST', 'PUT', 'DELETE', 'OPTIONS'],
-    allowedHeaders: ['Content-Type', 'Authorization', 'Cookie'],
-    credentials: true
-}));
