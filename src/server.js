9614f6d73209959740d2df7f5ccf10013a6c4e85
gabriel <contato.gabrielfeitosa@gmail.com>
2025-01-25 03:16:14 -0300

    remove custom CORS middleware in favor of existing cors package configuration

--- src/server.js
+++ src/server.js
@@ -13,14 +13,6 @@ app.use(cors({
     credentials: true
 }));
 
-app.use((req, res, next) => {
-    res.header("Access-Control-Allow-Origin", process.env.ORIGIN_URL);
-    res.header("Access-Control-Allow-Methods", "GET, POST, PUT, DELETE, OPTIONS");
-    res.header("Access-Control-Allow-Headers", "Content-Type, Authorization, Cookie");
-    res.header("Access-Control-Allow-Credentials", "true");
-    next();
-});
-
 app.use(express.json());
 app.use(cookieParser());
 app.use(todosRoutes);
