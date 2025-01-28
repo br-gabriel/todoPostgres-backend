406079653cc53de2039fe7c1ff85b327a179d739
gabriel <contato.gabrielfeitosa@gmail.com>
2025-01-28 16:31:20 -0300

    configure CORS middleware to use environment variable for origin and specify allowed methods and headers

--- src/server.js
+++ src/server.js
@@ -5,6 +5,13 @@ const userRoutes = require("./routes/user.routes");
 
 const app = express();
 
+app.use(cors({
+    origin: process.env.URL,
+    methods: ['GET', 'POST', 'PUT', 'DELETE', 'OPTIONS'],
+    allowedHeaders: ['Content-Type', 'Authorization', 'Cookie'],
+    credentials: true,
+}));
+
 app.use(express.json());
 app.use(cookieParser());
 app.use(todosRoutes);
