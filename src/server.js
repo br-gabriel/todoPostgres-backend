ed80aa8d3c843675e25fbe517513dc4bcd2e6021
gabriel <contato.gabrielfeitosa@gmail.com>
2025-01-28 17:03:08 -0300

    [TEST] implement CORS middleware with wildcard origin and specified methods and headers

--- src/server.js
+++ src/server.js
@@ -1,17 +1,30 @@
-// const cors = require("cors");
 const express = require("express");
+const cors = require("cors");
 const cookieParser = require("cookie-parser");
 const todosRoutes = require("./routes/todos.routes");
 const userRoutes = require("./routes/user.routes");
 
 const app = express();
 
-// app.use(cors({
-//     origin: process.env.URL,
-//     methods: ['GET', 'POST', 'PUT', 'DELETE', 'OPTIONS'],
-//     allowedHeaders: ['Content-Type', 'Authorization', 'Cookie'],
-//     credentials: true,
-// }));
+// app.use((req, res, next) => {
+//     res.header('Access-Control-Allow-Origin', 'https://todo-postgres-frontend.vercel.app');
+//     res.header('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE, OPTIONS');
+//     res.header('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept, Authorization, Cookie');
+//     res.header('Access-Control-Allow-Credentials', true);
+    
+//     if (req.method === 'OPTIONS') {
+//         return res.status(200).end();
+//     }
+    
+//     next();
+// });
+
+app.use(cors({
+    origin: '*',
+    methods: ['GET', 'POST', 'PUT', 'DELETE', 'OPTIONS'],
+    allowedHeaders: ['Content-Type', 'Authorization', 'Cookie'],
+    credentials: true,
+}));
 
 app.use(express.json());
 app.use(cookieParser());
